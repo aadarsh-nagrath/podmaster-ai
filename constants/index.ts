95985ca605522367df339c48d273ff827e2c53a0
aadarsh-nagrath <anagrath1@gmail.com>
2024-08-11 02:49:27 +0530

    index added, lib files added, structure fix

---
+++ constants/index.ts
@@ -0,0 +1,103 @@
+export const sidebarLinks = [
+  {
+    imgURL: "/icons/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/icons/discover.svg",
+    route: "/discover",
+    label: "Discover",
+  },
+  {
+    imgURL: "/icons/microphone.svg",
+    route: "/create-podcast",
+    label: "Create Podcast",
+  },
+];
+
+export const voiceDetails = [
+  {
+    id: 1,
+    name: "alloy",
+  },
+  {
+    id: 2,
+    name: "echo",
+  },
+  {
+    id: 3,
+    name: "fable",
+  },
+  {
+    id: 4,
+    name: "nova",
+  },
+  {
+    id: 5,
+    name: "onyx",
+  },
+  {
+    id: 6,
+    name: "shimmer",
+  },
+];
+
+export const podcastData = [
+  {
+    id: 1,
+    title: "The Joe Rogan Experience",
+    description: "A long form, in-depth conversation",
+    imgURL:
+      "https://lovely-flamingo-139.convex.cloud/api/storage/3106b884-548d-4ba0-a179-785901f69806",
+  },
+  {
+    id: 2,
+    title: "The Ranveer SHow TRS",
+    description: "Kya apne kabhi yati ko dekha hai?",
+    imgURL:
+      "https://lovely-flamingo-139.convex.cloud/api/storage/16fbf9bd-d800-42bc-ac95-d5a586447bf6",
+  },
+  {
+    id: 3,
+    title: "Koffee with Arpit Yadav",
+    description: "Join Michelle Obama in conversation",
+    imgURL:
+      "https://lovely-flamingo-139.convex.cloud/api/storage/60f0c1d9-f2ac-4a96-9178-f01d78fa3733",
+  },
+  {
+    id: 4,
+    title: "The Tech Talks Daily with Aadi",
+    description: "This is how the news should sound",
+    imgURL:
+      "https://lovely-flamingo-139.convex.cloud/api/storage/5ba7ed1b-88b4-4c32-8d71-270f1c502445",
+  },
+  {
+    id: 5,
+    title: "Metal music with Abhoy Bengoli",
+    description: "A long form, in-depth conversation",
+    imgURL:
+      "https://lovely-flamingo-139.convex.cloud/api/storage/ca7cb1a6-4919-4b2c-a73e-279a79ac6d23",
+  },
+  {
+    id: 6,
+    title: "Chess nights with Tushar Haldia ",
+    description: "Join Tushar in amusing chess matchs",
+    imgURL:
+      "https://lovely-flamingo-139.convex.cloud/api/storage/b8ea40c7-aafb-401a-9129-73c515a73ab5",
+  },
+  {
+    id: 7,
+    title: "IMPAULSIVE",
+    description: "A long form, in-depth conversation",
+    imgURL:
+      "https://lovely-flamingo-139.convex.cloud/api/storage/8a55d662-fe3f-4bcf-b78b-3b2f3d3def5c",
+  },
+  {
+    id: 8,
+    title: "Backchodi with Ekansh",
+    description: "This is how to fuck around by ekansh aneja",
+    imgURL:
+      "https://lovely-flamingo-139.convex.cloud/api/storage/221ee4bd-435f-42c3-8e98-4a001e0d806e",
+  },
+];
